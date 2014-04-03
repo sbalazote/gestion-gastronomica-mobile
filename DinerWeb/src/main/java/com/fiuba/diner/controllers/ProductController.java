db24a4c4b0d1b6d5113e21da61f502ee7be582e5
marianofl85 <marianofl85@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-03 03:33:15 +0000

    #TE-02 - Servicio base de conexión entre la aplicación Mobile y la aplicación Web: Cambio url del servicio de categories para hacerlo RESTful

--- DinerWeb/src/main/java/com/fiuba/diner/controllers/ProductController.java
+++ DinerWeb/src/main/java/com/fiuba/diner/controllers/ProductController.java
@@ -1,36 +1,27 @@
 package com.fiuba.diner.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fiuba.diner.model.Category;
 import com.fiuba.diner.service.CategoryService;
-import com.google.gson.Gson;
 
 @Controller
 public class ProductController {
 
-	private static final Gson gson = new Gson();
-
 	@Autowired
 	private CategoryService categoryService;
 
-	@RequestMapping(value = "/getCategories", method = RequestMethod.GET)
-	public void getCategories(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		this.writeResponse(response, this.categoryService.getAll());
-	}
-
-	private void writeResponse(HttpServletResponse response, Object json) throws IOException {
-		response.setContentType("application/json");
-		PrintWriter out = response.getWriter();
-		out.print(gson.toJson(json));
+	@RequestMapping(value = "/categories", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Category> getCategories() throws IOException {
+		return this.categoryService.getAll();
 	}
 
 }
--- DinerWeb/src/main/webapp/WEB-INF/views/menu.jsp
+++ DinerWeb/src/main/webapp/WEB-INF/views/menu.jsp
@@ -12,7 +12,7 @@
 			<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span>
 			<span class="icon-bar"></span> <span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="home.do"><spring:message code="common.appName"/></a>
+		<a class="navbar-brand" href="home"><spring:message code="common.appName"/></a>
 	</div>
 
 </nav>
