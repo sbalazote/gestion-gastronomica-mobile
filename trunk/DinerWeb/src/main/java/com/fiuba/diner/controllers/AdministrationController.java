07e21be3e30df8029b0205a98a2c5609cacf501c
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-04 13:54:59 +0000

    #TE-05: Creación de interfaz visual base de ABMs

--- trunk/DinerWeb/src/main/java/com/fiuba/diner/controllers/AdministrationController.java
+++ trunk/DinerWeb/src/main/java/com/fiuba/diner/controllers/AdministrationController.java
@@ -1,27 +1,22 @@
 package com.fiuba.diner.controllers;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fiuba.diner.model.Category;
 import com.fiuba.diner.service.CategoryService;
 
 @Controller
-public class ProductController {
+public class AdministrationController {
 
 	@Autowired
 	private CategoryService categoryService;
 
-	@RequestMapping(value = "/getCategories", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Category> getCategories() throws IOException {
-		return this.categoryService.getAll();
+	@RequestMapping(value = "/categoryAdministration", method = RequestMethod.GET)
+	public String home(ModelMap modelMap) throws Exception {
+		return "categoryAdministration";
 	}
 
 }
