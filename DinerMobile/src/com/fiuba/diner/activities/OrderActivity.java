60407d0500953fb4f550f254bbbeec68f8b1668c
marianofl85 <marianofl85@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-05 17:34:10 +0000

    #US-01 - Como usuario mozo quiero poder visualizar el listado de mesas y poder abrir una seleccionándola

---
+++ DinerMobile/src/com/fiuba/diner/activities/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.fiuba.diner.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fiuba.diner.R;
+
+public class OrderActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_order);
+	}
+
+}
---
+++ DinerMobile/src/com/fiuba/diner/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.fiuba.diner.constant;
+
+public class Constants {
+
+	public static final String AVAILABLE_STATE = "Disponible";
+	public static final String OPEN_STATE = "Abierta";
+
+}
