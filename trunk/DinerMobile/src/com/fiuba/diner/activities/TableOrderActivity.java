d654386516ca5c5d43c01f771c27df76f656be14
marianofl85 <marianofl85@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-06 20:21:13 +0000

    #TE-01 - Creación del ambiente de desarrollo: Vuelo el modo compatibilidad

--- trunk/DinerMobile/src/com/fiuba/diner/activities/TableOrderActivity.java
+++ trunk/DinerMobile/src/com/fiuba/diner/activities/TableOrderActivity.java
@@ -3,35 +3,34 @@ package com.fiuba.diner.activities;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
 import com.fiuba.diner.R;
 import com.fiuba.diner.adapters.ProductListAdapter;
-import com.fiuba.diner.fragments.WaiterFragment;
 import com.fiuba.diner.fragments.OrderFragment;
+import com.fiuba.diner.fragments.WaiterFragment;
 import com.fiuba.diner.model.Product;
 
-public class TableOrderActivity extends ActionBarActivity {
+public class TableOrderActivity extends Activity {
 
 	private List<Product> products;
 	private ProductListAdapter adapter;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_table_order);
+		this.setContentView(R.layout.activity_table_order);
 
 		// Se carga el nombre del mozo en el fragment
-		 ((WaiterFragment)getSupportFragmentManager().findFragmentById(R.id.FrgWaiter)).setWaiterName("JOSE");
-		 
+		((WaiterFragment) this.getFragmentManager().findFragmentById(R.id.FrgWaiter)).setWaiterName("JOSE");
+
 		// Se carga la mesa en el fragment
-		 ((OrderFragment)getSupportFragmentManager().findFragmentById(R.id.FrgOrder)).setTable("Mesa 8");	
-		 
+		((OrderFragment) this.getFragmentManager().findFragmentById(R.id.FrgOrder)).setTable("Mesa 8");
+
 		this.products = new ArrayList<Product>();
 		ListView listView = (ListView) this.findViewById(R.id.orderListView);
 
@@ -39,23 +38,11 @@ public class TableOrderActivity extends ActionBarActivity {
 		listView.setAdapter(this.adapter);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 	public void addProduct(View view) {
 		Intent intent = new Intent(this, CategoryListActivity.class);
 		this.startActivityForResult(intent, 1);
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == 1 && resultCode == RESULT_OK) {
