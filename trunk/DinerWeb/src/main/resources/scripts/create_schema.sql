3cb9db390d7b74aa3b0612000d0b35a35a04aadd
marianofl85 <marianofl85@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-10 19:35:17 +0000

    US-12 - Como usuario Encargado de Cocina quiero poder visualizar los pedidos que ingresen a la cocina: Actualizo scripts de base de datos

--- trunk/DinerWeb/src/main/resources/scripts/create_schema.sql
+++ trunk/DinerWeb/src/main/resources/scripts/create_schema.sql
@@ -2,17 +2,34 @@ START TRANSACTION;
 
 CREATE SCHEMA `diner`;
 
+CREATE TABLE `diner`.`waiter` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `active` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `diner`.`table_state` (
+	`id` int(8) NOT NULL AUTO_INCREMENT,
+	`description` varchar(30) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `diner`.`table` (
 	`id` int(8) NOT NULL AUTO_INCREMENT,
-	`description` varchar(100) NOT NULL,
+	`state_id` int(2) NOT NULL,
+	`waiter_id` int(8) DEFAULT NULL,
 	`active` bit(1) NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
+	KEY `fk_table_table_state_idx` (`state_id`),
+	CONSTRAINT `fk_table_table_state` FOREIGN KEY (`state_id`) REFERENCES `table_state` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	KEY `fk_table_waiter_idx` (`waiter_id`),
+	CONSTRAINT `fk_table_waiter` FOREIGN KEY (`waiter_id`) REFERENCES `waiter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `diner`.`order` (
 	`id` int(8) NOT NULL AUTO_INCREMENT,
-	`state` varchar(50) NOT NULL,
-	`date` datetime NOT NULL,
 	`customer_amount` int(2) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -30,14 +47,15 @@ CREATE TABLE `diner`.`subcategory` (
 	`active` bit(1) NOT NULL,
 	`category_id` int(8) NOT NULL,
 	PRIMARY KEY (`id`),
-	KEY `fk_product_category_idx` (`category_id`),
-	CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+	KEY `fk_subcategory_category_idx` (`category_id`),
+	CONSTRAINT `fk_subcategory_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `diner`.`product` (
 	`id` int(8) NOT NULL AUTO_INCREMENT,
 	`description` varchar(100) NOT NULL,
 	`price` decimal(6,2) NOT NULL,
+	`celiac_allowed` bit(1) NOT NULL,
 	`active` bit(1) NOT NULL,
 	`subcategory_id` int(8) NOT NULL,
 	PRIMARY KEY (`id`),
@@ -45,17 +63,31 @@ CREATE TABLE `diner`.`product` (
 	CONSTRAINT `fk_product_subcategory` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategory` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `diner`.`order_detail_state` (
+	`id` int(8) NOT NULL AUTO_INCREMENT,
+	`description` varchar(30) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `diner`.`order_detail` (
 	`id` int(8) NOT NULL AUTO_INCREMENT,
-	`comment` varchar(100) NOT NULL,
 	`amount` int(4) NOT NULL,
+	`comment` varchar(100) NOT NULL,
 	`order_id` int(8) NOT NULL,
 	`product_id` int(8) NOT NULL,
+	`state_id` int(2) NOT NULL,
+	`request_date` datetime NOT NULL,
+	`preparation_date` datetime NOT NULL,
+	`prepared_date` datetime NOT NULL,
+	`delivered_date` datetime NOT NULL,
+	`closed_date` datetime NOT NULL,
 	PRIMARY KEY (`id`),
 	KEY `fk_order_detail_order_idx` (`order_id`),
 	CONSTRAINT `fk_order_detail_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 	KEY `fk_order_detail_product_idx` (`product_id`),
-	CONSTRAINT `fk_order_detail_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+	CONSTRAINT `fk_order_detail_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+	KEY `fk_order_detail_order_detail_state_idx` (`state_id`),
+	CONSTRAINT `fk_order_detail_order_detail_state` FOREIGN KEY (`state_id`) REFERENCES `order_detail_state` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `diner`.`order_table` (
