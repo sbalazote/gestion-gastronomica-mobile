fd25acdc4c80403fb273448fc2bcc8fbd899a6a5
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-17 00:07:09 +0000

    US-06: Como usuario mozo quiero poder cancelar un pedido que no haya ingresado a la cocina: Eliminación de Expandible list y algunas modificaciones

--- DinerMobile/src/com/fiuba/diner/adapters/OrderListAdapter.java
+++ DinerMobile/src/com/fiuba/diner/adapters/OrderListAdapter.java
@@ -7,36 +7,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fiuba.diner.R;
-import com.fiuba.diner.model.OrderListItem;
+import com.fiuba.diner.helper.OrderStateHelper;
+import com.fiuba.diner.model.OrderDetail;
 
-public class OrderListAdapter extends ArrayAdapter<OrderListItem> {
+public class OrderListAdapter extends ArrayAdapter<OrderDetail> {
 
 	private final Context context;
-	private final List<OrderListItem> orderList;
+	private final List<OrderDetail> orderDetails;
 
-	public OrderListAdapter(Context context, List<OrderListItem> orderList) {
-		super(context, R.layout.order_list_item, orderList);
+	public OrderListAdapter(Context context, List<OrderDetail> orderDetails) {
+		super(context, R.layout.order_product_detail_list_item, orderDetails);
 		this.context = context;
-		this.orderList = orderList;
+		this.orderDetails = orderDetails;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.order_list_item, parent, false);
+		View rowView = inflater.inflate(R.layout.order_product_detail_list_item, parent, false);
 
-		EditText amountEditText = (EditText) rowView.findViewById(R.id.amountEditText);
-		amountEditText.setText(this.orderList.get(position).getAmount());
-		
 		TextView productTextView = (TextView) rowView.findViewById(R.id.productTextView);
-		productTextView.setText(this.orderList.get(position).getProduct().getDescription());
-		
+		productTextView.setText(this.orderDetails.get(position).getProduct().getDescription());
+
 		TextView productPriceTextView = (TextView) rowView.findViewById(R.id.productPriceTextView);
-		productPriceTextView.setText(this.orderList.get(position).getPrice().toString());
+		productPriceTextView.setText(this.orderDetails.get(position).getProduct().getPrice().toString());
+
+		OrderDetail orderDetail = this.orderDetails.get(position);
+		EditText productAmountEditText = (EditText) rowView.findViewById(R.id.productAmountEditText);
+		EditText productCommentEditText = (EditText) rowView.findViewById(R.id.productCommentEditText);
+		productAmountEditText.setText(String.valueOf(orderDetail.getAmount()));
+		productCommentEditText.setText(orderDetail.getComment());
+		TextView productState = (TextView) rowView.findViewById(R.id.stateTextView);
+		if (this.orderDetails.get(position) != null) {
+			if (this.orderDetails.get(position).getState() != null) {
+				productState.setText(String.valueOf(this.orderDetails.get(position).getState().getDescription()));
+				productAmountEditText.setEnabled(false);
+				productCommentEditText.setEnabled(false);
+			} else {
+				productState.setText(OrderStateHelper.NEW.getState().getDescription());
+				productAmountEditText.setEnabled(true);
+				productCommentEditText.setEnabled(true);
+			}
+		}
 
 		return rowView;
 	}
