009daa86bfb4e460c8c9d8f403537f8cce8c571a
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-04-29 15:40:30 +0000

    US-12: Como usuario Encargado de Cocina quiero poder visualizar los pedidos que ingresen a la cocina. US-16:Como usuario Encargado de Barra quiero poder visualizar los pedidos que ingresen a la barra. Actualizacion de las tablas automaticamente.

--- trunk/DinerWeb/src/main/webapp/js/form/bar/bar.js
+++ trunk/DinerWeb/src/main/webapp/js/form/bar/bar.js
@@ -1,53 +1,57 @@
 
 Bar = function() {
 	var orderDetailId;
-	$.ajax({
-		url: "getBarOrders",
-		type: "GET",
-		async: false,
-		success: function(response) {
-			var aaData = [];
-			for (var i = 0, l = response.length; i < l; ++i) {
-				var orderDetail = [];
-				
-				orderDetail.push(response[i].detail.id);
-				orderDetail.push(response[i].table.id);
-				orderDetail.push(response[i].detail.product.description);
-				orderDetail.push(response[i].detail.amount);
-				orderDetail.push(response[i].detail.comment);
-				//orderDetail.push(response[i].tables[0].waiter.surname + " " + response[i].tables[0].waiter.name );
-				orderDetail.push("Rodriguez");
-				
-				if (response[i].detail.requestDate) {
-					var requestDate = new Date(response[i].detail.requestDate);
-					orderDetail.push(requestDate.format("dd-mm-yyyy HH:MM:ss"));
-				} else {
-					orderDetail.push("");
-				}
-				
-				if (response[i].detail.state.id == 2) {
-					orderDetail.push("<strong><span style='color:blue'>" + response[i].detail.state.description + "</span></strong>");
-					orderDetail.push("<a href='javascript:void(0);' class='a-start'>Iniciar</a>");
-				} else if (response[i].detail.state.id == 3) {
-					orderDetail.push("<strong><span style='color:orange'>" + response[i].detail.state.description + "</span></strong>");
-					orderDetail.push("<a href='javascript:void(0);' class='a-end'>Finalizar</a>");
-				} else {
-					orderDetail.push("<strong><span style='color:green'>" + response[i].detail.state.description + "</span></strong>");
-					orderDetail.push("");
-				}
-				
-				aaData.push(orderDetail);
-			}
-			$('.datatable').dataTable({
-				"aaData": aaData,
-				"bDestroy": true,
-			    "iDisplayLength": -1,
-			    "aaSorting": [[ 0, "desc" ]] // Sort by first column descending
-			});
-		},
-		error: function(response) {
-		}
-	});
+	window.setInterval(function() {
+    	$.ajax({
+    		url: "getBarOrders",
+    		type: "GET",
+    		async: false,
+    		success: function(response) {
+    			var aaData = [];
+    			for (var i = 0, l = response.length; i < l; ++i) {
+    				var orderDetail = [];
+    				
+    				orderDetail.push(response[i].detail.id);
+    				orderDetail.push(response[i].table.id);
+    				orderDetail.push(response[i].detail.product.description);
+    				orderDetail.push(response[i].detail.amount);
+    				orderDetail.push(response[i].detail.comment);
+    				//orderDetail.push(response[i].tables[0].waiter.surname + " " + response[i].tables[0].waiter.name );
+    				orderDetail.push("Rodriguez");
+    				
+    				if (response[i].detail.requestDate) {
+    					var requestDate = new Date(response[i].detail.requestDate);
+    					orderDetail.push(requestDate.format("dd-mm-yyyy HH:MM:ss"));
+    				} else {
+    					orderDetail.push("");
+    				}
+    				
+    				if (response[i].detail.state.id == 2) {
+    					orderDetail.push("<strong><span style='color:blue'>" + response[i].detail.state.description + "</span></strong>");
+    					orderDetail.push("<a href='javascript:void(0);' class='a-start'>Iniciar</a>");
+    				} else if (response[i].detail.state.id == 3) {
+    					orderDetail.push("<strong><span style='color:orange'>" + response[i].detail.state.description + "</span></strong>");
+    					orderDetail.push("<a href='javascript:void(0);' class='a-end'>Finalizar</a>");
+    				} else {
+    					orderDetail.push("<strong><span style='color:green'>" + response[i].detail.state.description + "</span></strong>");
+    					orderDetail.push("");
+    				}
+    				
+    				aaData.push(orderDetail);
+    			}
+    			$('.datatable').dataTable({
+    				"aaData": aaData,
+    				"bDestroy": true,
+    			    "iDisplayLength": -1,
+    			    "aaSorting": [[ 0, "desc" ]] // Sort by first column descending
+    			});
+    		},
+    		error: function(response) {
+    		}
+    	});
+	}, 5000);
+	
+	
 	
 	$('#divTable').on("click", ".a-start", function() {
 		parent = $(this).parent().parent();
@@ -61,7 +65,7 @@ Bar = function() {
 		parent = $(this).parent().parent();
 		orderDetailId = parent.find("td:first-child").html();
 		changeState();
-		parent.find("td:eq(7)").html("<strong><span style='color:red'>Preparado</span></strong>");
+		parent.find("td:eq(7)").html("<strong><span style='color:green'>Preparado</span></strong>");
 		parent.find("td:eq(8)").html("");
 	});
 	
