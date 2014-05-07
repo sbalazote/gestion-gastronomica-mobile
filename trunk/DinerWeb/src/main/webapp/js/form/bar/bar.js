4726bf2cb5c0dade81e20427994c3d48fc8267ff
sbalazote <sbalazote@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-05-07 22:21:52 +0000

    US-12: Como usuario Encargado de Cocina quiero poder visualizar los pedidos que ingresen a la cocina. US-16: Como usuario Encargado de Barra quiero poder visualizar los pedidos que ingresen a la cocina. +Fix en Agregar vista de pedidos (lado Web). TE-03: Servicio base de notificaciones +Fix registration_id del device

--- trunk/DinerWeb/src/main/webapp/js/form/bar/bar.js
+++ trunk/DinerWeb/src/main/webapp/js/form/bar/bar.js
@@ -1,63 +1,65 @@
+function refreshTable() {
+	$.ajax({
+		url: "getBarOrders",
+		type: "GET",
+		async: false,
+		success: function(response) {
+			var aaData = [];
+			for (var i = 0, l = response.length; i < l; ++i) {
+				var orderDetail = [];
+				
+				orderDetail.push(response[i].detail.id);
+				orderDetail.push(response[i].table.id);
+				orderDetail.push(response[i].detail.product.description);
+				orderDetail.push(response[i].detail.amount);
+				orderDetail.push(response[i].detail.comment);
+				//orderDetail.push(response[i].tables[0].waiter.surname + " " + response[i].tables[0].waiter.name );
+				orderDetail.push("Rodriguez");
+				
+				if (response[i].detail.requestDate) {
+					var requestDate = new Date(response[i].detail.requestDate);
+					orderDetail.push(requestDate.format("dd-mm-yyyy HH:MM:ss"));
+				} else {
+					orderDetail.push("");
+				}
+				
+				if (response[i].detail.state.id == 2) {
+					orderDetail.push("<strong><span style='color:blue'>" + response[i].detail.state.description + "</span></strong>");
+					orderDetail.push("<a href='javascript:void(0);' class='a-start'>Iniciar</a>");
+				} else if (response[i].detail.state.id == 3) {
+					orderDetail.push("<strong><span style='color:orange'>" + response[i].detail.state.description + "</span></strong>");
+					orderDetail.push("<a href='javascript:void(0);' class='a-end'>Finalizar</a>");
+				} else {
+					orderDetail.push("<strong><span style='color:green'>" + response[i].detail.state.description + "</span></strong>");
+					orderDetail.push("");
+				}
+				if(response[i].detail.preparationStartDate != undefined && response[i].detail.preparationEndDate == undefined ){
+					var newDate = dateFormat(response[i].detail.preparationStartDate, "yyyy-mm-dd HH:MM:ss");
+					orderDetail.push("<abbr class='timeago' title='"+ newDate + "'>" + newDate + "</abbr>");
+				}else{
+					orderDetail.push("");
+				}
+				aaData.push(orderDetail);
+			}
+			$('.datatable').dataTable({
+				"aaData": aaData,
+				"bDestroy": true,
+			    "iDisplayLength": -1,
+			    "aaSorting": [[ 0, "desc" ]] // Sort by first column descending
+			});
+			$('.timeago').timeago();
+		},
+		error: function(response) {
+		}
+	});
+}
 
 Bar = function() {
 	var orderDetailId;
 	var orderDetailStartDate;
 	
-	window.setInterval(function() {
-		$.ajax({
-			url: "getBarOrders",
-			type: "GET",
-			async: false,
-			success: function(response) {
-				var aaData = [];
-				for (var i = 0, l = response.length; i < l; ++i) {
-					var orderDetail = [];
-					
-    				orderDetail.push(response[i].detail.id);
-    				orderDetail.push(response[i].table.id);
-    				orderDetail.push(response[i].detail.product.description);
-    				orderDetail.push(response[i].detail.amount);
-    				orderDetail.push(response[i].detail.comment);
-    				//orderDetail.push(response[i].tables[0].waiter.surname + " " + response[i].tables[0].waiter.name );
-    				orderDetail.push("Rodriguez");
-    				
-    				if (response[i].detail.requestDate) {
-    					var requestDate = new Date(response[i].detail.requestDate);
-    					orderDetail.push(requestDate.format("dd-mm-yyyy HH:MM:ss"));
-    				} else {
-    					orderDetail.push("");
-    				}
-    				
-    				if (response[i].detail.state.id == 2) {
-    					orderDetail.push("<strong><span style='color:blue'>" + response[i].detail.state.description + "</span></strong>");
-    					orderDetail.push("<a href='javascript:void(0);' class='a-start'>Iniciar</a>");
-    				} else if (response[i].detail.state.id == 3) {
-    					orderDetail.push("<strong><span style='color:orange'>" + response[i].detail.state.description + "</span></strong>");
-    					orderDetail.push("<a href='javascript:void(0);' class='a-end'>Finalizar</a>");
-    				} else {
-    					orderDetail.push("<strong><span style='color:green'>" + response[i].detail.state.description + "</span></strong>");
-    					orderDetail.push("");
-    				}
-    				if(response[i].detail.preparationStartDate != undefined && response[i].detail.preparationEndDate == undefined ){
-    					var newDate = dateFormat(response[i].detail.preparationStartDate, "yyyy-mm-dd HH:MM:ss");
-    					orderDetail.push("<abbr class='timeago' title='"+ newDate + "'>" + newDate + "</abbr>");
-    				}else{
-    					orderDetail.push("");
-    				}
-    				aaData.push(orderDetail);
-    			}
-    			$('.datatable').dataTable({
-    				"aaData": aaData,
-    				"bDestroy": true,
-    			    "iDisplayLength": -1,
-    			    "aaSorting": [[ 0, "desc" ]] // Sort by first column descending
-    			});
-    			$('.timeago').timeago();
-			},
-			error: function(response) {
-			}
-		});
-	}, 5000);
+	refreshTable();
+	window.setInterval(refreshTable, 5000);
 	
 	$('#divTable').on("click", ".a-start", function() {
 		parent = $(this).parent().parent();
