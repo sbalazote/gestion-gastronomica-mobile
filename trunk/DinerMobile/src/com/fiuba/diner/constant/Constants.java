4a56613d81d8fe1d15d24b64cd939395fec279fe
marianofl85 <marianofl85@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-05-13 02:49:05 +0000

    US-47 - Como usuario Administrador/Encargado de Cocina/Encargado de Barra/Cajero quiero poder identificarme al ingresar a la aplicación. Las URLs REST no pasan por spring security.

--- trunk/DinerMobile/src/com/fiuba/diner/constant/Constants.java
+++ trunk/DinerMobile/src/com/fiuba/diner/constant/Constants.java
@@ -2,10 +2,10 @@ package com.fiuba.diner.constant;
 
 public class Constants {
 
-	// public static final String WEBAPP_HOST = "http://10.0.2.2:8080/diner/";
-	// public static final String WEBAPP_HOST = "http://192.168.1.130:8080/diner/";
-	// public static final String WEBAPP_HOST = "http://10.254.210.92:8080/diner/";
-	public static final String WEBAPP_HOST = "http://192.168.1.130:8080/diner/";
+	// public static final String WEBAPP_HOST = "http://10.0.2.2:8080/diner/rest/";
+	// public static final String WEBAPP_HOST = "http://192.168.1.130:8080/diner/rest/";
+	// public static final String WEBAPP_HOST = "http://10.254.210.92:8080/diner/rest/";
+	public static final String WEBAPP_HOST = "http://192.168.0.102:8080/diner/rest/";
 
 	public static final int MIN_CUSTOMERS = 1;
 	public static final int MAX_CUSTOMERS = 99;
