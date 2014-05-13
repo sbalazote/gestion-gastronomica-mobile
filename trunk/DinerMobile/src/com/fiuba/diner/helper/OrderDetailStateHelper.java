19d60f1593a91bc24e4f7ef832e538f3cd753bc2
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-05-13 18:13:25 +0000

    US-26: Como usuario Administrador quiero poder generar el ticket con opciones de pago para las mesas que se están cerrando. Adecuaciones en la aplicación mobile de los estados de los pedidos

---
+++ trunk/DinerMobile/src/com/fiuba/diner/helper/OrderDetailStateHelper.java
@@ -0,0 +1,21 @@
+package com.fiuba.diner.helper;
+
+import com.fiuba.diner.model.OrderDetailState;
+
+public enum OrderDetailStateHelper {
+
+	NEW(1, "Nuevo"), REQUESTED(2, "Solicitado"), IN_PROCESS(3, "En preparación"), PREPARED(4, "Preparado"), DELIVERED(5, "Entregado");
+
+	private OrderDetailState state;
+
+	private OrderDetailStateHelper(Integer id, String description) {
+		this.state = new OrderDetailState();
+		this.state.setId(id);
+		this.state.setDescription(description);
+	}
+
+	public OrderDetailState getState() {
+		return this.state;
+	}
+
+}
--- trunk/DinerMobile/src/com/fiuba/diner/helper/OrderStateHelper.java
+++ trunk/DinerMobile/src/com/fiuba/diner/helper/OrderStateHelper.java
@@ -1,20 +1,20 @@
 package com.fiuba.diner.helper;
 
-import com.fiuba.diner.model.OrderDetailState;
+import com.fiuba.diner.model.OrderState;
 
 public enum OrderStateHelper {
 
-	NEW(1, "Nuevo"), REQUESTED(2, "Solicitado"), IN_PROCESS(3, "En preparación"), PREPARED(4, "Preparado"), DELIVERED(5, "Entregado");
+	OPEN(1, "Abierta"), CLOSED(2, "Cerrada"), BILL(3, "Facturada");
 
-	private OrderDetailState state;
+	private OrderState state;
 
 	private OrderStateHelper(Integer id, String description) {
-		this.state = new OrderDetailState();
+		this.state = new OrderState();
 		this.state.setId(id);
 		this.state.setDescription(description);
 	}
 
-	public OrderDetailState getState() {
+	public OrderState getState() {
 		return this.state;
 	}
 
