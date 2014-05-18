42a42331752a18a6b74acc0081f6a4666795393c
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-05-18 19:47:04 +0000

    #US-46: Como usuario mozo quiero poder identificarme al ingresar a la aplicación: se agrega la obtencion de la mac address del movil para ser enviado como id del movil.

--- trunk/DinerMobile/src/com/fiuba/diner/helper/SharedPreferencesHelper.java
+++ trunk/DinerMobile/src/com/fiuba/diner/helper/SharedPreferencesHelper.java
@@ -10,7 +10,7 @@ public class SharedPreferencesHelper {
 	private static final String PREFERENCES_FILE_NAME = "appData";
 	private static final String PROPERTY_REG_ID = "registration_id";
 	private static final String PROPERTY_APP_VERSION = "appVersion";
-	private static final String PROPERTY_EXPIRATION_TIME = "onServerExpirationTimeMs";
+	// private static final String PROPERTY_EXPIRATION_TIME = "onServerExpirationTimeMs";
 	private static final String PROPERTY_USER = "user";
 
 	private static SharedPreferences preferences;
@@ -47,33 +47,6 @@ public class SharedPreferencesHelper {
 	public static String getRegistrationId(Context context) {
 		SharedPreferencesHelper.preferences = context.getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
 		String registrationId = SharedPreferencesHelper.preferences.getString(PROPERTY_REG_ID, "");
-
-		if (registrationId.length() == 0) {
-			return "";
-		}
-
-		// String registeredUser = SharedPreferencesHelper.preferences.getString(PROPERTY_USER, "");
-		int registeredVersion = SharedPreferencesHelper.preferences.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-		long expirationTime = SharedPreferencesHelper.preferences.getLong(PROPERTY_EXPIRATION_TIME, -1);
-
-		// SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
-		// String expirationDate = sdf.format(new Date(expirationTime));
-
-		// "Registro GCM encontrado (usuario=" + registeredUser + ", version=" + registeredVersion + ", expira=" + expirationDate
-
-		int currentVersion = getAppVersion(context);
-
-		if (registeredVersion != currentVersion) {
-			// "Nueva versión de la aplicación."
-			return "";
-		} else if (System.currentTimeMillis() > expirationTime) {
-			// "Registro GCM expirado."
-			return "";
-			// } else if (!txtUsuario.getText().toString().equals(registeredUser)) {
-			// "Nuevo nombre de usuario."
-			// return "";
-		}
-
 		return registrationId;
 	}
 
