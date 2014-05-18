2b5fd936335043c8865dafa74557a83f6007f4bf
sbalazote <sbalazote@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-05-18 17:49:18 +0000

    US-46: Como usuario mozo quiero poder identificarme al ingresar a la aplicación. +agregado clase LoginTask que se encarga de validar el ingreso desde la app mobile de un usuario

--- DinerMobile/src/com/fiuba/diner/activities/Login.java
+++ DinerMobile/src/com/fiuba/diner/activities/Login.java
@@ -1,17 +1,19 @@
 package com.fiuba.diner.activities;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fiuba.diner.R;
 import com.fiuba.diner.helper.Caller;
+import com.fiuba.diner.model.User;
+import com.fiuba.diner.tasks.LoginTask;
 
-public class Login extends Activity implements Caller<Void> {
+public class Login extends Activity implements Caller<Boolean> {
 
 	private EditText usernameEditText;
 	private EditText passwordEditText;
@@ -34,25 +36,30 @@ public class Login extends Activity implements Caller<Void> {
 				String username = Login.this.usernameEditText.getText().toString();
 				String password = Login.this.passwordEditText.getText().toString();
 
-				if ((username.compareTo("Pi") == 0) && (password.compareTo("Sosa") == 0)) {
-					Intent intent = new Intent(Login.this.getApplicationContext(), HomeActivity.class);
-					intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-					Login.this.startActivity(intent);
-					Login.this.finish();
-				} else {
-					AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Login.this);
-					alertDialogBuilder.setMessage("Ingreso incorrecto. Intente nuevamente.");
-					alertDialogBuilder.setNeutralButton("Aceptar", null);
-					AlertDialog alertDialog = alertDialogBuilder.create();
-					alertDialog.show();
-				}
+				User user = new User();
+				user.setName(username);
+				user.setPassword(password);
+
+				new LoginTask(Login.this).execute(user);
+
 			}
 		});
 	}
 
 	@Override
-	public void afterCall(Void result) {
-		this.setView();
+	public void afterCall(Boolean result) {
+		if (result) {
+			Intent intent = new Intent(Login.this.getApplicationContext(), HomeActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			Login.this.startActivity(intent);
+			Login.this.finish();
+			this.setView();
+		} else {
+			Toast.makeText(this.getApplicationContext(), "Ingreso incorrecto. Intente nuevamente.", Toast.LENGTH_LONG).show();
+			Login.this.usernameEditText.setText("");
+			Login.this.passwordEditText.setText("");
+			Login.this.usernameEditText.requestFocus();
+		}
 	}
 
 	private void setView() {
