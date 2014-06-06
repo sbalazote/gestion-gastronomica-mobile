e7f09c2b4a793a3e3d191170c8ea2e51567cea7f
luchopal <luchopal@154be3ff-c729-eef4-45aa-a3cddb5913c5>
2014-06-06 19:53:01 +0000

    US-49: Como usuario Mozo quiero poder escanear códigos QR para validar los cupones de descuento. Captura de la lectura del qr en la app.

--- trunk/DinerMobile/src/com/fiuba/diner/constant/Constants.java
+++ trunk/DinerMobile/src/com/fiuba/diner/constant/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	// public static final String WEBAPP_HOST = "http://10.0.2.2:8080/diner/rest/";
 	// public static final String WEBAPP_HOST = "http://192.168.1.130:8080/diner/rest/";
 	// public static final String WEBAPP_HOST = "http://10.254.210.92:8080/diner/rest/";
-	public static final String WEBAPP_HOST = "http://10.80.38.111:8080/diner/rest/";
+	public static final String WEBAPP_HOST = "http://192.168.1.100:8080/diner/rest/";
 
 	public static final int MIN_CUSTOMERS = 1;
 	public static final int MAX_CUSTOMERS = 99;
@@ -19,6 +19,6 @@ public class Constants {
 
 	public static final String MAC = "00B0D086BBF7";
 
-	public static final int ZBAR_SCANNER_REQUEST = 0;
-	public static final int ZBAR_QR_SCANNER_REQUEST = 1;
+	public static final int ZBAR_SCANNER_REQUEST = 25;
+	public static final int ZBAR_QR_SCANNER_REQUEST = 26;
 }
